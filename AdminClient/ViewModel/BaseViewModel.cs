db09f99ee9b32ebc0b3270ea9f6a760dcd795c47
= <=>
2018-10-21 11:38:31 +0700

    fix error references project

---
+++ AdminClient/ViewModel/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AdminClient
+{
+    /// <summary>
+    /// A base view model that fires property changed event as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        
+    }
+}
